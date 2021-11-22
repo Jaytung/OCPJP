610c601adc636f6875f49794edc700b08f65e039
JayTung <fun8238@gmail.com>
2021-11-22 13:02:52 +0800

    add INSERT_customer to database

---
+++ VeryGoodBook_Lib/sql/INSERT_customer.sql
@@ -0,0 +1,5 @@
+SELECT * FROM vgb.customers;
+
+INSERT INTO customers 
+	(id, name, email, password, birthday, gender)
+    VALUES("A120568726","Chirs","ss123456@uuu.test.com.tw","wwww123456","1995-05-16",'M');
