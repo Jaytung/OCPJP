7b87a93d8decb5541a1712b4cdfb9029f43aad8f
Jay Tung <fun8238@gmail.com>
2021-12-30 19:31:15 +0800

    2021/12/30

--- VeryGoodBook_Lib/sql/SELECT_product.sql
+++ VeryGoodBook_Lib/sql/SELECT_product.sql
@@ -14,6 +14,9 @@ WHERE name LIKE '?' ;
  SELECT id, name, unit_price, stock, photo_url, description, size, shelf_date, unit_Discount FROM blackcake.product
 WHERE name LIKE '?' OR description LIKE '?';
 
+ SELECT id, name, unit_price, stock, photo_url, description, size, shelf_date, unit_Discount,category FROM blackcake.product
+WHERE category LIKE '糕點';
+
 SELECT id, name, unit_price, stock, photo_url, description, size, shelf_date, unit_Discount FROM blackcake.product ORDER BY shelf_date DESC LIMIT 2;
 SELECT id, name, unit_price, stock, photo_url, description, size, shelf_date, unit_Discount FROM blackcake.product ORDER BY shelf_date ASC LIMIT 3;
 
